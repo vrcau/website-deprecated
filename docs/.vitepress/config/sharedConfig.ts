ee69f8f258fed969bc39a3442548cb46b91e5191
Misaka-L <lipww1234@foxmail.com>
2023-01-31 11:25:56 +0800

    添加搜索，更新 VitePress 版本

--- docs/.vitepress/config/sharedConfig.ts
+++ docs/.vitepress/config/sharedConfig.ts
@@ -31,6 +31,54 @@ export const sharedConfig = defineConfig({
         link: "https://jq.qq.com/?_wv=1027&k=oH8yHGNS",
       },
     ],
+    algolia: {
+      appId: "WIER0DF0Z3",
+      apiKey: "169c47a8900c8fcfbb53ead616d3114b",
+      indexName: "yuxiaviation",
+      locales: {
+        zh: {
+          placeholder: "搜索文档",
+          translations: {
+            button: {
+              buttonText: "搜索文档",
+              buttonAriaLabel: "搜索文档",
+            },
+            modal: {
+              searchBox: {
+                resetButtonTitle: "清除查询条件",
+                resetButtonAriaLabel: "清除查询条件",
+                cancelButtonText: "取消",
+                cancelButtonAriaLabel: "取消",
+              },
+              startScreen: {
+                recentSearchesTitle: "搜索历史",
+                noRecentSearchesText: "没有搜索历史",
+                saveRecentSearchButtonTitle: "保存至搜索历史",
+                removeRecentSearchButtonTitle: "从搜索历史中移除",
+                favoriteSearchesTitle: "收藏",
+                removeFavoriteSearchButtonTitle: "从收藏中移除",
+              },
+              errorScreen: {
+                titleText: "无法获取结果",
+                helpText: "你可能需要检查你的网络连接",
+              },
+              footer: {
+                selectText: "选择",
+                navigateText: "切换",
+                closeText: "关闭",
+                searchByText: "搜索提供者",
+              },
+              noResultsScreen: {
+                noResultsText: "无法找到相关结果",
+                suggestedQueryText: "你可以尝试查询",
+                reportMissingResultsText: "你认为该查询应该有结果？",
+                reportMissingResultsLinkText: "点击反馈",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   head: [
     [
@@ -47,6 +95,14 @@ export const sharedConfig = defineConfig({
         content: "hagVbuxgC0yWzIxY53LQgx64neNoygvsQqR6rlJVYfI",
       },
     ],
+    [
+      "link",
+      {
+        rel: "preconnect",
+        href: "https://WIER0DF0Z3-dsn.algolia.net",
+        crossorign: ""
+      }
+    ]
   ],
   transformHtml: (_, id, { pageData }) => {
     if (!/[\\/]404\.html$/.test(id))
